8bc314e768aefb49c4988d1bb0c973ea94be781c
User <user@example.com>
2026-02-21 15:18:12 +0545

    signing in

--- app/admin/login/page.js
+++ app/admin/login/page.js
@@ -79,7 +79,7 @@ export default function AdminLogin() {
                                 type="password"
                                 required
                                 className="relative block w-full rounded-lg border-0 py-2.5 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
-                                placeholder="••••••••"
+                                placeholder="password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                             />
